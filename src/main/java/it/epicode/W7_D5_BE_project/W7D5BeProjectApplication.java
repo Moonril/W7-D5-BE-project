a2a20f16622e4b94893d73ec08f222eae896c9c9
Vincenza <vinni2690@hotmail.com>
2025-06-20 10:00:30 +0200

    initial

---
+++ src/main/java/it/epicode/W7_D5_BE_project/W7D5BeProjectApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.W7_D5_BE_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W7D5BeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W7D5BeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/W7_D5_BE_project/W7D5BeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.W7_D5_BE_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W7D5BeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
